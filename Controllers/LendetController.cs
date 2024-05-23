6106ff837f1b979b0d0616320f4ddc5c39327da7
LeartIstrefaj <leartistrefaj02@gmail.com>
2024-05-23 11:19:34 +0200

    nlog and subject controller implementation

---
+++ Controllers/LendetController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LendetController : ControllerBase
+    {
+        private readonly ILogger<LendetController> _logger;
+        public LendetController(ILogger<LendetController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("[action]")]
+        public string GetTest()
+        {
+            _logger.LogInformation("testi i pare");
+            return "Testi 1";
+        }
+    }
+}
